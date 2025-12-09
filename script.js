caa898fd2d215002edb9e7800bba7fe42d854b9a
Sergei Pronin <sergpetrpron@yandex.ru>
2025-12-10 03:31:42 +0500

    Add branch lesson02, change file script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const title
-const screens
-const screenPrice
-const rollback
-const fullPrice
-const adaptive
+let title = "myFirstRepo";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 75;
+let rollback = 50;
+let fullPrice = 80000;
+let adaptive = true;
 
-alert("Hello world!");
-console.log("Hello world in console!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юани");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юани");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback/100));
+
+
